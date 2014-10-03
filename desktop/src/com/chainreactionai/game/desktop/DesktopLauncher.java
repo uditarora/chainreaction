75766a50bb723c50b7640a2cbfb6db851757c6d2
Kartik Parnami <kartikparnami@gmail.com>
2014-10-03 19:09:55 +0530

    Initializing the project

---
+++ desktop/src/com/chainreactionai/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.chainreactionai.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.chainreactionai.game.ChainReactionAIGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ChainReactionAIGame(), config);
+	}
+}
---
+++ html/src/com/chainreactionai/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.chainreactionai.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.chainreactionai.game.ChainReactionAIGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new ChainReactionAIGame();
+        }
+}
