88edbb36a8f158ce156dbaadebe6849e69f8ac45
Kartik Parnami <kartikparnami@gmail.com>
2014-11-07 23:33:41 +0530

    Time for each move now logged. Checking of moves where ball already exists is now compulsory. Some utility functions added to GameBoard. Size of game changed for new version of graphics. Started graphics porting from images to drawing using libgdx.

--- desktop/src/com/chainreactionai/game/desktop/DesktopLauncher.java
+++ desktop/src/com/chainreactionai/game/desktop/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "ChainReaction";
-		config.width = 440;
+		config.width = 452;
 		config.height = 480;
 		new LwjglApplication(new ChainReactionAIGame(), config);
 	}
