bac838e39d1ddbe2e99db6ce1c011865cb43cf80
Kartik Parnami <kartikparnami@gmail.com>
2015-01-02 18:49:03 +0530

    NumPlayerScreen also has animation which is in continuation with MainMenuScreen.

--- core/src/com/chainreactionai/game/NumPlayersScreen.java
+++ core/src/com/chainreactionai/game/NumPlayersScreen.java
@@ -3,13 +3,26 @@
  */
 package com.chainreactionai.game;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -27,7 +40,6 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
  */
 public class NumPlayersScreen implements Screen {
 	SpriteBatch batch;
-	private OrthographicCamera camera;
 	private ChainReactionAIGame myGame;
 	final private int WIDTH_SCREEN = 448;
 	final private int HEIGHT_SCREEN = 645;
@@ -36,6 +48,13 @@ public class NumPlayersScreen implements Screen {
 	final private int WIDTH_SUBMIT_BUTTON = 100;
 	final private int HEIGHT_SUBMIT_BUTTON = 40;
 	private float heightUpscaleFactor, widthUpscaleFactor;
+	final private int INVERSE_CHANCES_OF_NEW_BALLS = 40;
+	final private int MAX_Z_DIST_OF_NEW_BALLS = 400;
+	final private int MIN_Z_DIST_OF_NEW_BALLS = 200;
+	final private int MAX_SPEED_OF_BALLS = 2;
+	final private int MIN_SPEED_OF_BALLS = 1;
+	final private int MAX_NUMBER_OF_BALLS_AT_A_MOMENT = 3;
+	private int numBalls;
 	private Stage stage = new Stage();
 	private Table table = new Table();
 	private int MAX_NUMBER_OF_PLAYERS = ChainReactionAIGame.MAX_NUMBER_PLAYERS, NUMBER_OF_DIFFICULTY_LEVELS = 10;
@@ -46,11 +65,46 @@ public class NumPlayersScreen implements Screen {
 	private TextButtonStyle submitButtonStyler;
 	private Slider numPlayerSlider;
 	private Label numPlayerLabel;
+	private Color[] colors;
+	private boolean animationInit = false;
+	// Trying 3D Graphics
+	private Model[] models;
+	private ModelInstance[] instances;
+	private ModelBatch modelBatch;
+	private PerspectiveCamera cam;
+	private Environment environment;
+	private ArrayList<Integer> startZPosition, distNow, xVal, yVal, color, speed;
+	private Random rand;
 	
 	public NumPlayersScreen(ChainReactionAIGame game) {
 		ChainReactionAIGame.currentScreen = 1;
 		myGame = game;
 		create();
+	}
+	
+	public NumPlayersScreen(ChainReactionAIGame game, ArrayList<Integer> xVal, ArrayList<Integer> yVal, ArrayList<Integer> color, ArrayList<Integer> startZPosition, ArrayList<Integer> distNow, ArrayList<Integer> speed, int numBalls) {
+		int i;
+		ChainReactionAIGame.currentScreen = 1;
+		myGame = game;
+		// Initialize ArrayLists
+		this.xVal = new ArrayList<Integer>();
+		this.yVal = new ArrayList<Integer>();
+		this.color = new ArrayList<Integer>();
+		this.startZPosition = new ArrayList<Integer>();
+		this.distNow = new ArrayList<Integer>();
+		this.speed = new ArrayList<Integer>();
+		this.numBalls = numBalls;
+		// Copy ArrayLists
+		for (i = 0; i < xVal.size(); i += 1) {
+			this.xVal.add(xVal.get(i));
+			this.yVal.add(yVal.get(i));
+			this.color.add(color.get(i));
+			this.startZPosition.add(startZPosition.get(i));
+			this.distNow.add(distNow.get(i));
+			this.speed.add(speed.get(i));
+		}
+		create();
+		animationInit = true;;
 	}
 
 	// Initialization function
@@ -62,10 +116,35 @@ public class NumPlayersScreen implements Screen {
 		// upscaled or downscaled according to the Screen Dimensions
 		heightUpscaleFactor = ((float)(ChainReactionAIGame.HEIGHT))/HEIGHT_SCREEN;
 		widthUpscaleFactor = ((float)(ChainReactionAIGame.WIDTH))/WIDTH_SCREEN;
-		// Show the world to be 440*480 no matter the
-		// size of the screen
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, WIDTH_SCREEN, HEIGHT_SCREEN);
+		// Initialize colors
+		colors = new Color[MAX_NUMBER_OF_PLAYERS];
+		colors[0] = Color.WHITE;
+		colors[1] = Color.BLUE;
+		colors[2] = Color.MAROON;
+		colors[3] = Color.ORANGE;
+		colors[4] = Color.PURPLE;
+		colors[5] = Color.GREEN;
+		// Trying 3D graphics
+		cam = new PerspectiveCamera(30, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		float camZ = ((float)1440*720/1240)*((float)Gdx.graphics.getHeight()/Gdx.graphics.getWidth());
+		cam.position.set(WIDTH_SCREEN/2, HEIGHT_SCREEN/2, camZ);
+	    cam.lookAt(WIDTH_SCREEN/2, HEIGHT_SCREEN/2, 0);
+	    cam.near = 1f;
+	    cam.far = 4000f;
+	    cam.update();
+	    // Building models for spheres of different colors
+	    ModelBuilder modelBuilder = new ModelBuilder();
+		models = new Model[MAX_NUMBER_OF_PLAYERS];
+		instances = new ModelInstance[MAX_NUMBER_OF_PLAYERS];
+		for (int i = 0; i < MAX_NUMBER_OF_PLAYERS; i += 1) {
+			models[i] = modelBuilder.createSphere(25f, 25f, 25f, 30, 30, new Material(ColorAttribute.createDiffuse(colors[i])), Usage.Position | Usage.Normal | Usage.TextureCoordinates);
+			instances[i] = new ModelInstance(models[i]);
+		}
+		modelBatch = new ModelBatch();
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+        rand = new Random();
 		// Initializing and adding the title to Table.
 		title = new Label("Choose number of players", skin);
 		title.setFontScale((1+(heightUpscaleFactor-1)/2));
@@ -107,14 +186,82 @@ public class NumPlayersScreen implements Screen {
 
 	@Override
 	public void render(float delta) {
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 		Gdx.gl.glClearColor(((float)(15)/255), ((float)(15)/255), ((float)(15)/255), 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		if (animationInit) {
+			modelBatch.begin(cam);
+			createAnimation();
+			drawAnimation();
+			modelBatch.end();
+		}
 		numPlayerLabel.setText(String.valueOf((int)(numPlayerSlider.getValue())));
-		stage.act();
+		stage.act(delta);
 		stage.draw();
 		if (Gdx.input.isKeyJustPressed(Keys.BACK)) {
-			myGame.setScreen(new MainMenuScreen(myGame));
+			myGame.setScreen(new MainMenuScreen(myGame, xVal, yVal, color, startZPosition, distNow, speed, numBalls));
 		}
+	}
+	
+	private void createAnimation() {
+		int newOrNot, xCoord, yCoord, zCoord, speedOfBall;
+		newOrNot = rand.nextInt(INVERSE_CHANCES_OF_NEW_BALLS);
+		if ((newOrNot == 0 || (numBalls == 0)) && numBalls < MAX_NUMBER_OF_BALLS_AT_A_MOMENT) {
+			if (numBalls == 0) {
+				clearBallsList();
+			}
+			zCoord = rand.nextInt(MAX_Z_DIST_OF_NEW_BALLS);
+			if (zCoord < MIN_Z_DIST_OF_NEW_BALLS) {
+				zCoord += MIN_Z_DIST_OF_NEW_BALLS;
+			}
+			startZPosition.add(zCoord);
+			distNow.add(0);
+			xCoord = rand.nextInt(WIDTH_SCREEN/2);
+			if (xCoord >= (WIDTH_SCREEN/4)) {
+				xCoord = (xCoord + (WIDTH_SCREEN/2));
+			}
+			xVal.add(xCoord);
+			yCoord = rand.nextInt(HEIGHT_SCREEN);
+			yVal.add(yCoord);
+			color.add(rand.nextInt(MAX_NUMBER_OF_PLAYERS));
+			speedOfBall = rand.nextInt(MAX_SPEED_OF_BALLS) + 1;
+			if (speedOfBall < MIN_SPEED_OF_BALLS) {
+				speedOfBall += MIN_SPEED_OF_BALLS;
+			}
+			speed.add(speedOfBall);
+			numBalls += 1;
+		}
+	}
+	
+	private void drawAnimation() {
+		int xCoord, yCoord, zCoord;
+		for (int i = 0; i < startZPosition.size(); i += 1) {
+			xCoord = xVal.get(i);
+			if (xCoord != -1) {
+				yCoord = yVal.get(i);
+				zCoord = -startZPosition.get(i) + distNow.get(i);
+				instances[color.get(i)].transform.setTranslation(xCoord, yCoord, zCoord);
+				modelBatch.render(instances[color.get(i)], environment);
+				distNow.set(i, distNow.get(i) + speed.get(i));
+				if (distNow.get(i) - startZPosition.get(i) > 0) {
+					deleteBallFromList(i);
+				}
+			}
+		}
+	}
+	
+	private void deleteBallFromList(int index) {
+		xVal.set(index, -1);
+		numBalls -= 1;
+	}
+	
+	private void clearBallsList () {
+		xVal.clear();
+		yVal.clear();
+		color.clear();
+		startZPosition.clear();
+		distNow.clear();
+		speed.clear();
 	}
 
 	@Override
