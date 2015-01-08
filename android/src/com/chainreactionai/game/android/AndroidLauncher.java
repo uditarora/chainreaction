ad74ea3878f400c588305791bfeb64d83b4ab3f0
pratyushsharma <pratyushsharma@outlook.com>
2015-01-08 13:01:58 +0530

    Edited android launcher

--- android/src/com/chainreactionai/game/android/AndroidLauncher.java
+++ android/src/com/chainreactionai/game/android/AndroidLauncher.java
@@ -13,144 +13,128 @@ import com.google.android.gms.games.Games;
 import com.google.example.games.basegameutils.GameHelper;
 import com.google.example.games.basegameutils.GameHelper.GameHelperListener;
 
-public class AndroidLauncher extends AndroidApplication implements IGoogleServices {
-	
+public class AndroidLauncher extends AndroidApplication implements
+		IGoogleServices {
+
 	private GameHelper _gameHelper;
 	private final static int REQUEST_CODE_UNUSED = 9002;
 
-	
-//	@Override
-//	protected void onCreate (Bundle savedInstanceState) {
-//		super.onCreate(savedInstanceState);
-//		
-//		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-//		
-//		// Changes in config to disable Accelerometer and Compass so as
-//		// to conserve battery.
-//		config.useAccelerometer = false;
-//		config.useCompass = false;
-//		
-//		// Initialize the game
-//		initialize(new ChainReactionAIGame(), config);
-//	}
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
-	super.onCreate(savedInstanceState);
-
-	// Create the GameHelper.
-	_gameHelper = new GameHelper(this, GameHelper.CLIENT_GAMES);
-	_gameHelper.enableDebugLog(false);
-
-	GameHelperListener gameHelperListener = new GameHelper.GameHelperListener()
-	{
-	@Override
-	public void onSignInSucceeded()
-	{
-	}
+	// @Override
+	// protected void onCreate (Bundle savedInstanceState) {
+	// super.onCreate(savedInstanceState);
+	//
+	// AndroidApplicationConfiguration config = new
+	// AndroidApplicationConfiguration();
+	//
+	// // Changes in config to disable Accelerometer and Compass so as
+	// // to conserve battery.
+	// config.useAccelerometer = false;
+	// config.useCompass = false;
+	//
+	// // Initialize the game
+	// initialize(new ChainReactionAIGame(), config);
+	// }
 
 	@Override
-	public void onSignInFailed()
-	{
-	}
-	};
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-	_gameHelper.setup(gameHelperListener);
-	
-	
-	super.onCreate(savedInstanceState);
-	
-	AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-	
-	// Changes in config to disable Accelerometer and Compass so as
-	// to conserve battery.
-	config.useAccelerometer = false;
-	config.useCompass = false;
-	
-	// Initialize the game
-	initialize(new ChainReactionAIGame(this), config);
+		// Create the GameHelper.
+		_gameHelper = new GameHelper(this, GameHelper.CLIENT_GAMES);
+		_gameHelper.enableDebugLog(false);
 
-	// The rest of your onCreate() code here...
-	}
-	
-	@Override
-	public void signIn()
-	{
-	try
-	{
-	runOnUiThread(new Runnable()
-	{
-	//@Override
-	public void run()
-	{
-	_gameHelper.beginUserInitiatedSignIn();
-	}
-	});
-	}
-	catch (Exception e)
-	{
-	Gdx.app.log("MainActivity", "Log in failed: " + e.getMessage() + ".");
-	}
+		GameHelperListener gameHelperListener = new GameHelper.GameHelperListener() 
+		{
+			@Override
+			public void onSignInSucceeded() {
+			}
+
+			@Override
+			public void onSignInFailed() {
+			}
+		};
+
+		_gameHelper.setup(gameHelperListener);
+
+		super.onCreate(savedInstanceState);
+
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+
+		// Changes in config to disable Accelerometer and Compass so as
+		// to conserve battery.
+		config.useAccelerometer = false;
+		config.useCompass = false;
+
+		// Initialize the game
+		initialize(new ChainReactionAIGame(this), config);
+
+		// The rest of your onCreate() code here...
 	}
 
 	@Override
-	public void signOut()
-	{
-	try
-	{
-	runOnUiThread(new Runnable()
-	{
-	//@Override
-	public void run()
-	{
-	_gameHelper.signOut();
-	}
-	});
-	}
-	catch (Exception e)
-	{
-	Gdx.app.log("MainActivity", "Log out failed: " + e.getMessage() + ".");
-	}
+	public void signIn() {
+		try {
+			runOnUiThread(new Runnable() {
+				// @Override
+				public void run() {
+					_gameHelper.beginUserInitiatedSignIn();
+				}
+			});
+		} catch (Exception e) {
+			Gdx.app.log("MainActivity", "Log in failed: " + e.getMessage()
+					+ ".");
+		}
 	}
 
 	@Override
-	public void rateGame()
-	{
-	// Replace the end of the URL with the package of your game
-	String str ="https://play.google.com/store/apps/details?id=org.fortheloss.plunderperil";
-	startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(str)));
+	public void signOut() {
+		try {
+			runOnUiThread(new Runnable() {
+				// @Override
+				public void run() {
+					_gameHelper.signOut();
+				}
+			});
+		} catch (Exception e) {
+			Gdx.app.log("MainActivity", "Log out failed: " + e.getMessage()
+					+ ".");
+		}
 	}
 
 	@Override
-	public void submitScore(long score)
-	{
-	if (isSignedIn() == true)
-	{
-	Games.Leaderboards.submitScore(_gameHelper.getApiClient(), getString(R.string.leaderboard_id), score);
-	startActivityForResult(Games.Leaderboards.getLeaderboardIntent(_gameHelper.getApiClient(), getString(R.string.leaderboard_id)), REQUEST_CODE_UNUSED);
-	}
-	else
-	{
-	// Maybe sign in here then redirect to submitting score?
-	}
+	public void rateGame() {
+		// Replace the end of the URL with the package of your game
+		String str = "https://play.google.com/store/apps/details?id=org.fortheloss.plunderperil";
+		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(str)));
 	}
 
 	@Override
-	public void showScores()
-	{
-	if (isSignedIn() == true)
-	startActivityForResult(Games.Leaderboards.getLeaderboardIntent(_gameHelper.getApiClient(), getString(R.string.leaderboard_id)), REQUEST_CODE_UNUSED);
-	else
-	{
-	// Maybe sign in here then redirect to showing scores?
-	}
+	public void submitScore(long score) {
+		if (isSignedIn() == true) {
+			Games.Leaderboards.submitScore(_gameHelper.getApiClient(),
+					getString(R.string.leaderboard_id), score);
+			startActivityForResult(Games.Leaderboards.getLeaderboardIntent(
+					_gameHelper.getApiClient(),
+					getString(R.string.leaderboard_id)), REQUEST_CODE_UNUSED);
+		} else {
+			// Maybe sign in here then redirect to submitting score?
+		}
 	}
 
 	@Override
-	public boolean isSignedIn()
-	{
-	return _gameHelper.isSignedIn();
+	public void showScores() {
+		if (isSignedIn() == true)
+			startActivityForResult(Games.Leaderboards.getLeaderboardIntent(
+					_gameHelper.getApiClient(),
+					getString(R.string.leaderboard_id)), REQUEST_CODE_UNUSED);
+		else {
+			// Maybe sign in here then redirect to showing scores?
+		}
 	}
-	
+
+	@Override
+	public boolean isSignedIn() {
+		return _gameHelper.isSignedIn();
+	}
+
 }
